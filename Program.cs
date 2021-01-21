35d2d4e4ab4ae547feeaa651315c1dae028ae910
hello <hello@LAPTOP-NA2F1EVT>
2021-01-21 12:08:01 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VisualStudio2019_CsharpGitDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
